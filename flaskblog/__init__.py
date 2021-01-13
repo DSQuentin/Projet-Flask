dab3366fe25b797afa378859b44103e1f00b97ab
Maetis <41517613+Maetis0511@users.noreply.github.com>
2021-01-13 14:29:46 +0100

    organisation du projet

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7b306650e4ca93bc44c0c3f814f191d1'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'site.db')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
